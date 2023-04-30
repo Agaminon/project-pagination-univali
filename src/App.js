2cdcb71477df02c8a4efa009e94ba70da777857d
Agaminon <131570562+Agaminon@users.noreply.github.com>
2023-04-30 19:37:21 +1200

    Add files via upload
    projeto paginação

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+
+function App() {
+  return (
+    <div class='container'>
+ <header>
+    <h2>Paginação</h2>
+
+    <ul class="pagination">
+      <div>
+      <li class="page-item"><a class="page-link" href="#">Anterior</a></li>
+      </div>
+      <div>
+      <li class="page-item"><a class="page-link" href="#">...</a></li>
+      </div>
+      <div>
+      <li class="page-item"><a class="page-link" href="#">2</a></li>
+      </div>
+      <div>
+      <li class="page-item"><a class="page-link" href="#">3</a></li>
+      </div>
+      <div>
+      <li class="page-item"><a class="page-link" href="#">4</a></li>
+      </div>
+      <div>
+      <li class="page-item"><a class="page-link" href="#">5</a></li>
+      </div>
+      <div>
+      <li class="page-item"><a class="page-link" href="#">6</a></li>
+      </div>
+      <div>
+      <li class="page-item"><a class="page-link" href="#">...</a></li>
+      </div>
+      <div>
+      <li class="page-item"><a class="page-link" href="#">Próximo</a></li>
+      </div>
+    </ul>
+
+    </header>
+    </div>
+  );
+}
+
+export default App;
